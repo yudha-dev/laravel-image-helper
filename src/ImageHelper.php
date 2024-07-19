43dfcb000fdeb3fa464a0aa5fa78e7c21b5188d3
yudha-dev <ydnd21@gmail.com>
2024-07-20 02:37:17 +0700

    fix directory

--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -2,23 +2,27 @@
 
 namespace Yudhadev\LaravelImageHelper;
 
+use Illuminate\Http\UploadedFile;
+
 class ImageHelper
 {
-    public static function upload($file, $path)
+    public static function upload(UploadedFile $file, $path)
     {
         // Membuat direktori jika belum ada
-        if (!file_exists($path)) {
-            mkdir($path, 0777, true);
+        if (!file_exists(public_path($path))) {
+            mkdir(public_path($path), 0777, true);
         }
 
-        $imageName = uniqid() . '.' . pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
-        $file->move($path, $imageName);
+        $imageName = uniqid() . '.' . $file->getClientOriginalExtension();
+        $file->move(public_path($path), $imageName);
 
         return $imageName;
     }
 
-    public static function deleteImage($imagePath)
+    public static function deleteImage($imageName, $path)
     {
+        $imagePath = public_path($path . '/' . $imageName);
+
         if (file_exists($imagePath)) {
             unlink($imagePath);
         }
