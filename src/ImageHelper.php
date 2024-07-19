4ac5ce6350c7bcf7793ab3ea8f1c50f40aa0ab2c
yudha-dev <ydnd21@gmail.com>
2024-07-20 01:31:25 +0700

    update

--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -7,20 +7,20 @@ class ImageHelper
     public static function upload($file, $path)
     {
         // Membuat direktori jika belum ada
-        if (!file_exists(public_path($path))) {
-            mkdir(public_path($path), 0777, true);
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
         }
 
-        $imageName = uniqid() . '.' . $file->getClientOriginalExtension();
-        $file->move(public_path($path), $imageName);
+        $imageName = uniqid() . '.' . pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
+        $file->move($path, $imageName);
 
         return $imageName;
     }
 
-    public static function deleteImage($imageName)
+    public static function deleteImage($imagePath)
     {
-        if (file_exists(public_path($imageName))) {
-            unlink(public_path($imageName));
+        if (file_exists($imagePath)) {
+            unlink($imagePath);
         }
     }
 }
